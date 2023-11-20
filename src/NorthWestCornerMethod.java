5c822558f9e866de68d6f8832998238e2d689378
2IMT <daniil_t04@mail.ru>
2023-11-20 04:34:56 +0300

    Implement solve and tweak the docs

--- src/NorthWestCornerMethod.java
+++ src/NorthWestCornerMethod.java
@@ -3,6 +3,17 @@ public class NorthWestCornerMethod {
     private final Matrix costsCoefficients;
     private final Matrix demandCoefficients;
 
+    /**
+     * Constructs a north west corner method solver with specified supply, demand,
+     * and costs.
+     * The sum of supply coefficients is assumed to be equal to the sum of demand
+     * coefficients.
+     * If this condition is not satisfied, the behavior of solver is undefined.
+     * 
+     * @param coefficientsOfSupply Supply coefficients
+     * @param coefficientsOfCosts  Cost coefficients
+     * @param coefficientsOfDemand Demand coefficients
+     */
     public NorthWestCornerMethod(Matrix coefficientsOfSupply,
             Matrix coefficientsOfCosts,
             Matrix coefficientsOfDemand) {
@@ -22,11 +33,34 @@ public class NorthWestCornerMethod {
     }
 
     /**
-     *
+     * This method does not change the state of solver, thus, when invoked
+     * several times, the result is consistent between calls.
+     * 
      * @return initial solution X0 obtained using North-West corner method
      */
     public Matrix solve() {
-        Matrix initialSolution = new Matrix(costsCoefficients.getRows(), costsCoefficients.getColumns());
-        return initialSolution;
+        Matrix solution = new Matrix(costsCoefficients.getRows(), costsCoefficients.getColumns());
+        Matrix demand = new Matrix(demandCoefficients);
+        Matrix supply = new Matrix(supplyCoefficients);
+
+        int i = 0;
+        int j = 0;
+        while (i < solution.getRows() && j < solution.getRows()) {
+            int allocation = min(demandCoefficients.getElement(j, 0), supplyCoefficients.getElement(i, 0));
+            demand.setElement(j, 0, demand.getElement(j, 0) - allocation);
+            supply.setElement(i, 0, supply.getElement(i, 0) - allocation);
+
+            solution.setElement(i, j, allocation);
+
+            if (demand.getElement(j, 0) == 0) {
+                j += 1;
+            }
+
+            if (supply.getElement(i, 0) == 0) {
+                i += 1;
+            }
+        }
+
+        return solution;
     }
 }
