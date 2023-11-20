7f4bd21023673dba7d462c9a901a6abb9047048a
2IMT <daniil_t04@mail.ru>
2023-11-20 03:45:13 +0300

    Implement min and make it static

--- src/NorthWestCornerMethod.java
+++ src/NorthWestCornerMethod.java
@@ -4,8 +4,8 @@ public class NorthWestCornerMethod {
     private final Matrix demandCoefficients;
 
     public NorthWestCornerMethod(Matrix coefficientsOfSupply,
-                                 Matrix coefficientsOfCosts,
-                                 Matrix coefficientsOfDemand){
+            Matrix coefficientsOfCosts,
+            Matrix coefficientsOfDemand) {
         supplyCoefficients = coefficientsOfSupply;
         costsCoefficients = coefficientsOfCosts;
         demandCoefficients = coefficientsOfDemand;
@@ -17,15 +17,15 @@ public class NorthWestCornerMethod {
      * @param b 2nd double number
      * @return minimum from numbers a and b
      */
-    private double min(double a, double b){
-        return 0;
+    private static double min(double a, double b) {
+        return Math.min(a, b);
     }
 
     /**
      *
      * @return initial solution X0 obtained using North-West corner method
      */
-    public Matrix solve(){
+    public Matrix solve() {
         Matrix initialSolution = new Matrix(costsCoefficients.getRows(), costsCoefficients.getColumns());
         return initialSolution;
     }
